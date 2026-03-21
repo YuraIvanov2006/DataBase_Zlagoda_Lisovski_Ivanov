f5218d7520f4902783679118e1cc14af7faa4f39
lisovskyi-arsenii <lisovskyiarsenii@gmail.com>
2026-03-21 15:49:14 +0200

    add a few methods in employee repository

--- backend/src/main/java/org/lisovskyi_ivanov/backend/repository/employee_repos/EmployeeRepository.java
+++ backend/src/main/java/org/lisovskyi_ivanov/backend/repository/employee_repos/EmployeeRepository.java
@@ -1,18 +1,27 @@
 package org.lisovskyi_ivanov.backend.repository.employee_repos;
 
 import org.lisovskyi_ivanov.backend.entity.Employee;
+import org.lisovskyi_ivanov.backend.enums.Role;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository {
     List<Employee> findAll();
+    List<Employee> findAllByRole(Role role);
+    List<Employee> findAllCashiers();
+    List<Employee> findAllManagers();
+    List<Employee> findAllByCity(String city);
+    List<Employee> findAllOrderBySurname();
+    List<Employee> findAllBySalaryGreaterThan(BigDecimal salary);
     Optional<Employee> findById(Long id);
     Optional<Employee> findBySurname(String surname);
     Optional<Employee> findBySurnameAndName(String surname, String name);
     Optional<Employee> findBySurnameAndNameAndPatronymic(String surname, String name, String patronymic);
     Employee save(Employee employee);
+    void update(Employee employee);
+    boolean existsById(Long id);
     void deleteById(Long id);
     void delete(Employee employee);
-//    void deleteAll();
 }
--- backend/src/main/java/org/lisovskyi_ivanov/backend/repository/employee_repos/EmployeeRepositoryImpl.java
+++ backend/src/main/java/org/lisovskyi_ivanov/backend/repository/employee_repos/EmployeeRepositoryImpl.java
@@ -2,17 +2,22 @@ package org.lisovskyi_ivanov.backend.repository.employee_repos;
 
 import lombok.RequiredArgsConstructor;
 import org.lisovskyi_ivanov.backend.entity.Employee;
+import org.lisovskyi_ivanov.backend.enums.Role;
 import org.lisovskyi_ivanov.backend.mapping.mapper.EmployeeRowMapper;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class EmployeeRepositoryImpl implements EmployeeRepository {
+    private static final String TABLE_NAME = "employees";
+    private static final String SELECT_ALL = "SELECT * FROM " + TABLE_NAME;
+
     private final JdbcTemplate jdbc;
     private final EmployeeRowMapper employeeRowMapper;
 
@@ -23,8 +28,44 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     }
 
     @Override
+    public List<Employee> findAllByRole(Role role) {
+        String sql = SELECT_ALL + " WHERE empl_role = ?";
+        return jdbc.query(sql, employeeRowMapper, role.getRoleName());
+    }
+
+    @Override
+    public List<Employee> findAllCashiers() {
+        String sql = SELECT_ALL + " WHERE empl_role = 'cashier'";
+        return jdbc.query(sql, employeeRowMapper);
+    }
+
+    @Override
+    public List<Employee> findAllManagers() {
+        String sql = SELECT_ALL + " WHERE empl_role = 'manager'";
+        return jdbc.query(sql, employeeRowMapper);
+    }
+
+    @Override
+    public List<Employee> findAllByCity(String city) {
+        String sql = SELECT_ALL + " WHERE empl_city = ?";
+        return jdbc.query(sql, employeeRowMapper, city);
+    }
+
+    @Override
+    public List<Employee> findAllOrderBySurname() {
+        String sql = SELECT_ALL + " ORDER BY empl_surname";
+        return jdbc.query(sql, employeeRowMapper);
+    }
+
+    @Override
+    public List<Employee> findAllBySalaryGreaterThan(BigDecimal salary) {
+        String sql = SELECT_ALL + " WHERE salary > ?";
+        return jdbc.query(sql, employeeRowMapper, salary);
+    }
+
+    @Override
     public Optional<Employee> findById(Long id) {
-        String sql = "SELECT * FROM employees WHERE id_employee = ?";
+        String sql = SELECT_ALL + " WHERE id_employee = ?";
         try {
             var employee = jdbc.queryForObject(sql, employeeRowMapper, id);
             return Optional.of(employee);
@@ -35,7 +76,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     @Override
     public Optional<Employee> findBySurname(String surname) {
-        String sql = "SELECT * FROM employees WHERE empl_surname = ?";
+        String sql = SELECT_ALL + " WHERE empl_surname = ?";
         try {
             var employee = jdbc.queryForObject(sql, employeeRowMapper, surname);
             return Optional.of(employee);
@@ -46,7 +87,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     @Override
     public Optional<Employee> findBySurnameAndName(String surname, String name) {
-        String sql = "SELECT * FROM employees WHERE empl_surname = ? AND empl_name = ?";
+        String sql = SELECT_ALL + " WHERE empl_surname = ? AND empl_name = ?";
         try {
             var employee = jdbc.queryForObject(sql, employeeRowMapper, surname, name);
             return Optional.of(employee);
@@ -57,7 +98,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
 
     @Override
     public Optional<Employee> findBySurnameAndNameAndPatronymic(String surname, String name, String patronymic) {
-        String sql = "SELECT * FROM employees WHERE empl_surname = ? AND empl_name = ? AND empl_patronymic = ?";
+        String sql = SELECT_ALL + " WHERE empl_surname = ? AND empl_name = ? AND empl_patronymic = ?";
         try {
             var employee = jdbc.queryForObject(sql, employeeRowMapper, surname, name, patronymic);
             return Optional.of(employee);
@@ -85,6 +126,35 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     }
 
     @Override
+    public void update(Employee employee) {
+        String sql =
+        """
+        UPDATE employees SET
+            empl_surname = :emplSurname,
+            empl_name = :emplName,
+            empl_patronymic = :emplPatronymic,
+            empl_role = :emplRole,
+            salary = :salary,
+            date_of_birth = :dateOfBirth,
+            date_of_start = :dateOfStart,
+            empl_phone_number = :phoneNumber,
+            empl_city = :city,
+            empl_street = :street,
+            empl_zip_code = :zipCode
+        WHERE id_employee = :idEmployee;
+        """;
+
+        jdbc.update(sql, employee);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        String sql = "SELECT COUNT(*) FROM employees WHERE id_employee = ?";
+        Integer count = jdbc.queryForObject(sql, Integer.class, id);
+        return count != null && count > 0;
+    }
+
+    @Override
     public void deleteById(Long id) {
         String sql = "DELETE FROM employees WHERE id_employee = ?";
         jdbc.update(sql, id);
@@ -94,10 +164,4 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     public void delete(Employee employee) {
         deleteById(employee.getIdEmployee());
     }
-
-//    @Override
-//    public void deleteAll() {
-//        String sql = "DELETE FROM employees";
-//        jdbc.update(sql);
-//    }
 }
